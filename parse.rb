33a437aa75c1dcce5bd415507b176e74e6310ab6
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-06-24 00:07:59 -0400

    making sense of results

--- parse.rb
+++ parse.rb
@@ -1,24 +1,59 @@
 
 require "sentimentalizer"
+require "pry"
 
-class Analyzer
-  def initialize
-    Sentimentalizer.setup
+require "active_support/all"
+
+#
+#
+#
+
+class Sentiment
+  def self.all
+    ["POSITIVE", "NEGATIVE", "NEUTRAL"].map do |sentiment|
+      {:face => "Sentiment::#{sentiment}".constantize, :name => sentiment.downcase }
+    end
   end
 
-  def process(phrase)
-    Sentimentalizer.analyze phrase
+  # @param [String] face
+  # @example Sentiment.translate(":)")
+  def self.find_by_face(face)
+    all.find{|s| s[:face] == face }
   end
 end
 
-analyzer = Analyzer.new
+puts Sentiment.all
+puts Sentiment.find_by_face(":)")
+
+
+#
+#
+#
+
+Sentimentalizer.setup
+
 [
   "i am so happy",
   "i am so sad",
   "its sad to see you go",
   "its sad to see you go, but i'm the most happy i've ever been now that you are leaving",
-  "you're the most hoorible person I've ever met",
+  "you're the most horrible person I've ever met",
+  "you're the most horrible person I've ever met, and I never wish to see you or speak to you ever again",
+  "i'm angry",
+  "i'm interested",
+  "i'm interesting",
+  "i'm satisfied",
+  "i'm satisfied for now",
+  "i'm satisfied, for now",
+  "the mail arrives on tuesdays",
+  "the mail was late today",
+  "if the mail is late one more time, i'm going to be upset",
+  "liars",
+  "the dog smiles from ear to ear",
+  "the cat smiles from ear to ear"
 ].each do |phrase|
-  s = analyzer.process(phrase)
-  puts s.to_json
+  classification = JSON.parse(Sentimentalizer.analyze(phrase, true)).to_h
+  #classification["sentiment"] = translate(classification["sentiment"])
+  sentiment = Sentiment.find_by_face(classification["sentiment"])
+  puts "#{classification["probability"].round(2)} -- #{sentiment[:name].upcase}"
 end
